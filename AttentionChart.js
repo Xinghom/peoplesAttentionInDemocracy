decc6a6d3d6d584d59128bed34898dd7cf5c471a
Xinghom <Xinghom@users.noreply.github.com>
2018-03-17 13:31:27 -0700

    let's do it

---
+++ AttentionChart.js
@@ -0,0 +1,16 @@
+var width = 1080;
+var height = 800;
+
+var svg = d3.select("svg")
+            .attr("margin", "auto")
+            .attr("width", width)
+            .attr("height", height);
+
+// axis
+// area chart
+// 5 toggles
+// legend
+// brush
+// rightside information display
+
+// 干巴爹！
