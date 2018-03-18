ac17a706c66af527ea94b4eca1ba8b5939abd535
Xinghom <Xinghom@users.noreply.github.com>
2018-03-17 17:16:54 -0700

    read multi-csv

--- AttentionChart.js
+++ AttentionChart.js
@@ -14,3 +14,16 @@ var svg = d3.select("svg")
 // rightside information display
 
 // 干巴爹！
+
+d3.queue()
+    .defer(d3.csv, "usEng.csv")
+    .defer(d3.csv, "Chinese.csv")
+    .defer(d3.csv, "Heb.csv")
+    .defer(d3.csv, "rus.csv")
+    .defer(d3.csv, "spa.csv")
+    .await(function(error, usEng_file, chi_file, heb_file, rus_file, spa_file) {
+        if (error) {console.error("csv reading error: " + error)}
+        else {
+            // here we have read already! do your work here! yeah^^
+        }
+});
